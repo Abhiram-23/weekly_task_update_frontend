c9ce7815be477a8a2d5cc6eccbe6d7973682f0ee
Abhiram <pabhi2000@gmail.com>
2025-06-13 22:29:09 -0700

    code refactored

--- src/components/EntriesList.js
+++ src/components/EntriesList.js
@@ -1,18 +1,5 @@
 import { useEffect, useState, useRef } from "react";
 
-function getDayOfWeek(dateStr) {
-  const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  return days[new Date(dateStr).getDay()];
-}
-
 export default function EntriesList({ session, backendUrl }) {
   const [entries, setEntries] = useState([]);
   const [editingId, setEditingId] = useState(null);
